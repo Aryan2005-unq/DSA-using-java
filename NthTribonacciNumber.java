70b5688a4322d59b1eee45d910f1067fde0c2626
Aryan Prakash Koli <aryankoli10c10@gmail.com>
2026-01-22 19:44:02 +0530

    Implement Nth Tribonacci number calculation

---
+++ NthTribonacciNumber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int tribonacci(int n) {
+       int t0 = 0 , t1 = 1, t2 = 1;
+       if(n==0){
+        return 0;
+       } 
+       if(n<=2){
+        return 1;
+       }
+       for(int i=3;i<=n;i++){
+        int temp = t2;
+        t2 = temp + t0 + t1;
+        t0 = t1;
+        t1 = temp;
+       }
+       return t2;
+    }
+}
