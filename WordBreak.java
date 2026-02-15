93f573676780dca23dc3b0528acaa7b458a13c61
Aryan Prakash Koli <aryankoli10c10@gmail.com>
2026-02-15 12:40:16 +0530

    Implement word break algorithm in WordBreak.java

---
+++ WordBreak.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        Set<String> wordList = new HashSet<>(wordDict);
+        for(int i=1;i<=s.length();i++){
+            for(int j=i-1;j>=0;j--){
+                if(dp[j] && wordList.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        } 
+        return dp[s.length()];
+    }
+}
